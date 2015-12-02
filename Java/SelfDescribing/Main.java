fc235d3a1021cd6722883c40085050ea3f2f3212
couryrr <coury.richards@gmail.com>
2015-12-01 22:13:55 -0500

    Removed the class files and finshed SelfDescibingNumbers

--- Java/SelfDescribing/Main.java
+++ Java/SelfDescribing/Main.java
@@ -8,8 +8,7 @@ import java.util.*;
  **/
 class Main{
     //Kicking it all off
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) throws FileNotFoundException, IOException{
             FileReader fr = new FileReader(args[0]);
             BufferedReader bf = new BufferedReader(fr);
 
@@ -25,33 +24,29 @@ class Main{
                     numMap.put(counter, value - 48);//The index(number) and value(occurance)
                     counter++;
                 }
-                System.out.println("+-------------------------------+");
-                System.out.println(line);
-                Iterator iter = numMap.entrySet().iterator();
+                Iterator<Map.Entry<Integer, Integer>> iter = numMap.entrySet().iterator();
                 boolean isSelfDescribing = true;
                 while(iter.hasNext()){
-                    Map.Entry pair = (Map.Entry) iter.next();
+                    Map.Entry<Integer, Integer> pair = iter.next();
                     int occurance = 0;
-                    System.out.println("Key: " + pair.getKey() + " Value: " + pair.getValue());
                     for(char value : values){
                         if(value - 48 == pair.getKey()){
                             occurance++;
                         }
                     }
                     if(!(occurance == pair.getValue())){
-                        System.out.println('1');
-                    } else {
-                        System.out.println('0');
+                        isSelfDescribing = false;
+                        break;
                     }
 
-                    System.out.println(occurance);
-                    numMap = new HashMap<Integer, Integer>();
+               }
+               numMap = new HashMap<Integer, Integer>();
+               if(isSelfDescribing){
+                   System.out.println("1");
+               } else {
+                   System.out.println("0");
                }
                line = bf.readLine();
             }
         }
-        catch(IOException e){
-            System.out.println(e);
-        }
     }
-}
